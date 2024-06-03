392b8283f0af5d546bf0284e7399378561ffa755
Serge Deugoue <deugouserge@gmail.com>
2024-06-03 04:54:02 +0200

    update tests and factories

--- tests/test_core/test_api.py
+++ tests/test_core/test_api.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .factories.core_factories import OrderFactory, ProductFactory, UserFactory
+from tests.factories.core_factories import OrderFactory, ProductFactory, UserFactory
 
 
 class APITests(APITestCase):
@@ -20,11 +20,11 @@ class APITests(APITestCase):
             "price": "15.00",
             "stock": 100,
         }
-        response = self.client.post("/api/products", data, format="json")
+        response = self.client.post("/api/products/", data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_product_list(self):
-        response = self.client.get("/api/products", format="json")
+        response = self.client.get("/api/products/", format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreaterEqual(len(response.data), 1)
 
@@ -35,15 +35,15 @@ class APITests(APITestCase):
             "quantity": 1,
             "status": "pending",
         }
-        response = self.client.post("/api/orders", data, format="json")
+        response = self.client.post("/api/orders/", data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_order_list(self):
-        response = self.client.get("/api/orders", format="json")
+        response = self.client.get("/api/orders/", format="json")
         assert response.status_code == 200
         assert len(response.data) > 1
 
     def test_get_profile_list(self):
-        response = self.client.get("profile", format="json")
+        response = self.client.get("/api/profile/", format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreaterEqual(len(response.data), 1)
